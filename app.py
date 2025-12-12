a67893a5d493f594bd9017f823bb65407bb1e475
santiagolopezR <santiago.negr08@gmail.com>
2025-12-12 05:39:04 -0500

    Update app.py

--- app.py
+++ app.py
@@ -8,4 +8,9 @@ from streamlit_webrtc import webrtc_streamer, WebRtcMode, RTCConfiguration
 st.title("📱 Lector de Códigos de Barras")
 camara=st.camera_input("foto")
 if camara:
+    imagen= Imagen.open(camara)
     st.image(camara)
+    img_cv= cv2.cvtColor(imagen,cv2.COLOR_RGB2BGR)
+    
+    
+
