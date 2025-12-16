00aa178190f8caca1a7021ab14c075c27b99efa9
santiagolopezR <santiago.negr08@gmail.com>
2025-12-16 15:18:16 -0500

    Update app.py

--- app.py
+++ app.py
@@ -132,3 +132,35 @@ if buscar:
     ]
     st.dataframe(resultados, use_container_width=True)
     st.info(f"🔎 Resultados encontrados: {len(resultados)}")
+
+
+
+
+
+
+from fastapi import FastAPI
+from pydantic import BaseModel
+from datetime import datetime
+
+app = FastAPI()
+
+ultimo_scan = {
+    "code": None,
+    "timestamp": None
+}
+
+class Scan(BaseModel):
+    code: str
+
+@app.post("/scan")
+def recibir_scan(scan: Scan):
+    ultimo_scan["code"] = scan.code
+    ultimo_scan["timestamp"] = datetime.now().isoformat()
+    return {"status": "ok", "code": scan.code}
+
+@app.get("/last")
+def ultimo_codigo():
+    return ultimo_scan
+
+
+
